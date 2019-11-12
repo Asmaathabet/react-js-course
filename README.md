e6cb1649d3cbc8d600e6f5d6760fdc916205e45a
asmaa <asmathabet2009@hotmail.com>
2019-11-12 21:31:00 +0200

    add Hello component to explain jsx

--- README.md
+++ README.md
@@ -10,5 +10,6 @@ Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 ## Main Contents:
 
 * Functional Component VS Class Component 
-
 * Hooks
+* JSX
+
---
+++ src/components/Hello.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Hello = () =>{
+    return (
+        //JSX
+        <div>
+            <h1> Hello from JSX Explanation</h1>
+        </div>
+    )
+}
+
+export default Hello; 
