d13f9dac852ffbf270893324f5244677efa40458
asmaa <asmathabet2009@hotmail.com>
2019-11-12 21:54:21 +0200

    add createElement method to create jsx elements

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 const Hello = () =>{
-    return (
-        //JSX
-        <div>
-            <h1> Hello from JSX Explanation</h1>
-        </div>
-    )
+    // return (
+    //     //JSX
+    //     <div>
+    //         <h1> Hello from JSX Explanation</h1>
+    //     </div>
+    // )
+
+    return React.createElement('div', null, React.createElement( 'h1', null, 'Hello from JSX !'))
 }
 
 export default Hello; 
