ca6050d379034647577dc16cff73d924cbd276fb
asmaa <asmathabet2009@hotmail.com>
2019-11-13 12:43:28 +0200

    use this with event handler in the class , it doesnt work

--- README.md
+++ README.md
@@ -17,6 +17,7 @@ Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 * setState 
 * Destructring
 * Event Handling
+* Binding Event Handlers
 
 
 ## Use Extension ES7 React/Redux/GraphQL/React-Native snippets 
--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
 
-import Greet from './components/Greet';
-import Welcome from './components/Welcome';
-import Hello from './components/Hello';
-import Message from './components/Message';
-import Counter from './components/Counter';
-import FunctionClick from './components/FunctionClick';
-import ClassClick from './components/ClassClick';
+// import Greet from './components/Greet';
+// import Welcome from './components/Welcome';
+// import Hello from './components/Hello';
+// import Message from './components/Message';
+// import Counter from './components/Counter';
+// import FunctionClick from './components/FunctionClick';
+// import ClassClick from './components/ClassClick';
+import EventBind from './components/EventBind';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <FunctionClick />
+      <EventBind />
+      {/* <FunctionClick />
       <ClassClick />
       <Counter />
       <Greet name="Asmaa"  username="asmaa"><p1> Welcome !</p1> </Greet>
@@ -20,7 +22,7 @@ function App() {
       <Greet name="Alaa" username="alaa" ><button> click here </button></Greet>
       <Welcome name="Salma"  username="salma100" > <h1>Hi from Class</h1> </Welcome>
       <Hello />
-      <Message />
+      <Message /> */}
     </div>
   );
 }
