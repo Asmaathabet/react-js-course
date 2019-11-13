9435db132609a47aa8b980f17bf56b323e8e19f0
asmaa <asmathabet2009@hotmail.com>
2019-11-13 11:04:49 +0200

    add state to Message class

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,8 +1,14 @@
 import React , { Component } from 'react'; 
 
 class Message extends Component {
+    constructor () {
+        super(); 
+        this.state = {
+           message : 'Welcome Visitor'
+        }
+    }
     render (){
-        return <h1>  Hello  Visitor </h1>
+        return <h1>  Welcome  Visitor </h1>
 
          }
 }
