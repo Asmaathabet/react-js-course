469598e6f10472a92e4afaa73377a3a0f3d9e95c
asmaa <asmathabet2009@hotmail.com>
2019-11-13 12:10:20 +0200

    make class destructring

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,13 +1,26 @@
 import React , { Component } from 'react'; 
 
+// class Welcome extends Component {
+//     render (){
+//         return (
+//         <div>
+//             <h1> 
+//                 Hello  {this.props.name} a.k.a {this.props.username} 
+//             </h1>
+//             {this.props.children}
+//         </div>
+//         ) }
+// }
+
 class Welcome extends Component {
     render (){
+        const {name, username, children} = this.props; 
         return (
         <div>
             <h1> 
-                Hello  {this.props.name} a.k.a {this.props.username} 
+                Hello  {name} a.k.a {username} 
             </h1>
-            {this.props.children}
+            {children}
         </div>
         ) }
 }
