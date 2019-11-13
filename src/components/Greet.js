81bedc780fc8a9af3295c697cfd4ed30f192f838
asmaa <asmathabet2009@hotmail.com>
2019-11-13 12:03:58 +0200

    use destructring in pass props

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -4,17 +4,29 @@ import React from 'react';
 //     return <h1> Hello From React !</h1>
 // }
 
-const Greet = (props) => {
+// const Greet = (props) => {
+//     // props is immutable => can't be assign or take another value
+// return (    
+//     // a.k.a => also known as
+//     <div>
+//         <h1> 
+//             Hello {props.name} a.k.a  {props.username} 
+//         </h1>
+//         {props.children}
+//     </div>
+// )
+// } 
+
+const Greet = ({name, username, children}) => {
     // props is immutable => can't be assign or take another value
 return (    
     // a.k.a => also known as
     <div>
         <h1> 
-            Hello {props.name} a.k.a  {props.username} 
+            Hello {name} a.k.a  {username} 
         </h1>
-        {props.children}
+        {children}
     </div>
 )
 } 
-
 export default Greet;
