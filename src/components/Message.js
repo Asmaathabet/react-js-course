8382eddd56cb0fa2a19a3f7b12d7293b5da051c1
asmaa <asmathabet2009@hotmail.com>
2019-11-13 11:12:22 +0200

    handel the state by event handling method

--- src/components/Message.js
+++ src/components/Message.js
@@ -7,8 +7,19 @@ class Message extends Component {
            message : 'Welcome Visitor'
         }
     }
+
+    changeMessage(){
+        this.setState({
+            message: 'Thank you for subscribing'
+        })
+    }
     render (){
-        return <h1> {this.state.message} </h1>
+        return (
+            <div>
+                <h1>  {this.state.message} </h1>
+                <button onClick={() => this.changeMessage() }>Subscribe</button>
+            </div>
+        )
 
          }
 }
