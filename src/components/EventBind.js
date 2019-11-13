a6c377d78eb5df7fb50d7b0c3375287bafdb7621
asmaa <asmathabet2009@hotmail.com>
2019-11-13 12:58:31 +0200

    Binding Event Handlers

--- src/components/EventBind.js
+++ src/components/EventBind.js
@@ -7,18 +7,39 @@ class EventBind extends Component {
         this.state = {
              message : 'Hello'
         }
+     // Approach 3   
+    // using binding the constructor better than using it in Rendering . why?
+    // because it happened once  [most Recommended]
+
+        // this.clickHandler = this.clickHandler.bind(this);
     }
-    clickHandler() {
+    // clickHandler() {
+    //     this.setState({
+    //         message : 'Goodbye !'
+    //     })
+    //     console.log(this);
+    // }
+
+
+    // Aprroach 4  -Using Arrow function property
+    clickHandler = () => {
         this.setState({
             message : 'Goodbye !'
-        })
+        }) 
     }
+
     render() {
         
         return (
             <div>
                 <div>{this.state.message }</div>
                 {/* this will not work and this => undefined */}
+                {/* <button onClick={this.clickHandler}> Click</button>  */}
+                {/* The  Approach  1  for binding */}
+                {/* <button onClick={this.clickHandler.bind(this)}> Click</button> */}
+                {/* The Approach 2  by using arrow function  */}
+                {/* <button onClick={() => this.clickHandler()}> Click</button> */}
+                {/* The Approach 3 and it found in the React documentation => add to constructor */}
                 <button onClick={this.clickHandler}> Click</button>
             </div>
         )
