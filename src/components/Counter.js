e367ed55ce1de76399e7183120d04853f519a021
asmaa <asmathabet2009@hotmail.com>
2019-11-13 11:44:40 +0200

    create increase by five using increase on function as it is

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -8,13 +8,22 @@ class Counter extends Component {
              count: 0
         }
     }
-    increment(){
+    incrementOne(){
         // setState => Asynchronous so the value in the console less than the value in state
         this.setState({
         count: this.state.count +1
         }, 
         ()=> console.log('Callback Value', this.state.count) )
         console.log(this.state.count)     
+    }
+
+    incrementFive(){
+        this.incrementOne();
+        this.incrementOne();
+        this.incrementOne();
+        this.incrementOne();
+        this.incrementOne();
+       
     }
     
     render() {
@@ -23,7 +32,7 @@ class Counter extends Component {
                 <div>
                     Count - {this.state.count}
                 </div>
-                <button onClick={() => this.increment()}> Increment</button>
+                <button onClick={() => this.incrementFive()}> Increment</button>
             </div>
         )
     }
