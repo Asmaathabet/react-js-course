a17780a38dfca023cc25c018405aa71ef7475e9c
asmaa <asmathabet2009@hotmail.com>
2019-11-15 09:59:54 +0200

    use map with array of objects

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -1,16 +1,41 @@
 import React from 'react'
 
 function NameList() {
-    const names = ['Ali', 'Ahmed' , 'Mohammed']
-    const nameList = names.map(name => <h2>{name}</h2>)
+    // const names = ['Ali', 'Ahmed' , 'Mohammed']
+    // const nameList = names.map(name => <h2>{name}</h2>)
+    const persons = [
+        {
+            id: 1,
+            name: 'Ali',
+            age: 30,
+            skill: 'React'
+        },
+        {
+            id: 2 ,
+            name: 'Ahmed',
+            age: 25,
+            skill:'Angular'
+        },
+        {
+            id: 3,
+            name: 'Mohammed',
+            age: 22 ,
+            skill: 'Vue'
+        },
+    ]
+    const PersonList = persons.map(person => 
+    <h2> I am {person.name}. I am {person.age} years old . I know {person.skill} </h2>
+    )
     return (
         <div>
+            
             {/* <h2>{names[0]}</h2>
             <h2>{names[1]}</h2>
             <h2>{names[2]}</h2> */}
-            {
+            {/* {
               nameList  
-            }
+            } */}
+            {PersonList}
         </div>
     )
 }
